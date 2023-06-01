b05d4f17f93b2c4006b6093d34cd228f15f09f24
tliner-friaca-dev <tlinerfc@hotmail.com>
2023-05-31 21:45:55 -0300

    first commit

---
+++ src/main/java/br/com/java9Features/Java9FeaturesApplication.java
@@ -0,0 +1,18 @@
+package br.com.java9Features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java9FeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java9FeaturesApplication.class, args);
+
+		System.out.println("\n----------------------");
+		System.out.println("ENTROU NA APLICAÇÃO!!!");
+		System.out.println("----------------------\n");
+		
+	}
+
+}
---
+++ src/test/java/br/com/java9Features/Java9FeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java9Features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java9FeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
