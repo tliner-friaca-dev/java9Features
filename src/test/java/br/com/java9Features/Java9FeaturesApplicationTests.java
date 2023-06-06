cc5e192354d42fa3e8fdbe7a0f3be01942a20ab3
tliner-friaca-dev <tlinerfc@hotmail.com>
2023-06-06 19:36:23 -0300

    Refatoração

--- src/test/java/br/com/java9Features/Java9FeaturesApplicationTests.java
+++ src/test/java/br/com/java9Features/Java9FeaturesApplicationTests.java
@@ -1,13 +1,65 @@
 package br.com.java9Features;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Java9FeaturesApplicationTests {
 
+private static final Optional<Object> OPTIONAL_EMPTY = Optional.empty();
+
+	/* 
+	*Static Factory Methods; List - Set- Map
+	- Optional.empty
+    - Optional.of
+    - Optional.ofNullable
+	- Collections.unmodifiableList
+	- List.of
+
+ */
 	@Test
-	void contextLoads() {
+	void optionalEmpty_retornaVazio_quandoSucesso() {
+		Optional<Integer> opt = Optional.empty();
+		assertEquals(OPTIONAL_EMPTY, opt);
 	}
 
+	@Test
+	void optionalOf_retornaValorIniciado_quandoSucesso() {
+		Optional<Integer> opt = Optional.of(10);
+		assertEquals(10, opt.get());
+	}
+
+	@Test
+	void optionalOfNullable_retornaVazio_quandoSucesso() {
+		Optional<Integer> opt = Optional.ofNullable(null);
+		assertEquals(OPTIONAL_EMPTY, opt);
+	}
+
+	@Test
+	void collectionsUnmodifiableList_retornaExcecaoAoTentarAdicionarValorEmListaImutavel_quandoSucesso() {
+		List<Integer> list = new ArrayList<>();
+		List<Integer> novaLista = Collections.unmodifiableList(list);
+		assertThrows(UnsupportedOperationException.class, () -> adicionaValorNaLista(novaLista).orElseThrow());
+	}
+
+	public Optional<List<Integer>> adicionaValorNaLista(List<Integer> novaLista){
+		novaLista.add(10);
+		return Optional.of(novaLista);
+	}
+	
+	@Test
+	void listOf_retornaListaInicializada_quandoSucesso() {
+		List<Integer> list = List.of(1, 2, 3, 4, 5);
+
+
+		assertEquals(List.of(1, 2, 3, 4, 5), list);
+	}
 }
